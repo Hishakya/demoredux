df31e533cf6e1cf8e22765c9721ddcbc3d20fe7a
Hi ! Shakya <hishakya@His-MacBook-Air.local>
2022-05-12 01:19:32 +0530

    gud1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
-
+import "./App.css";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import ChangeColor from "./components/ChangeColor";
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Profile />
+      <Login />
+      <ChangeColor />
     </div>
   );
 }
